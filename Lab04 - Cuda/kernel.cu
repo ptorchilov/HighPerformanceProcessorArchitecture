127d468ae15cca64f05013c4737df0b27cc14bbe
p.torchilov <p.torchilov@gmail.com>
2014-05-02 02:16:37 +0300

    Final lab05 with old comment code.

--- Lab04 - Cuda/kernel.cu
+++ Lab04 - Cuda/kernel.cu
@@ -2,24 +2,21 @@
 #include <conio.h>
 #include <time.h>
 #include <stdlib.h>
-#include "cuda.h"
+#include <cuda.h>
 #include "cuda_runtime.h"
 #include "device_launch_parameters.h"
 
+#define threads_number 512
+#define blocks_number 8
+#define N threads_number * blocks_number
 
-#define N 1024 * 32
-
-bool isEqual(int *cpu, int *gpu) 
+bool isEqual(int cpu, int gpu) 
 {
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < N; j++) {
-            if (cpu[i * N + j] != gpu[i * N + j]) {
-                return false;
-            }
-        }
+    if (cpu == gpu) {
+        return true;
     }
 
-    return true;
+    return false;
 }
 
 void cpu_compare(int *a, int *b, int *c)
@@ -27,71 +24,92 @@ void cpu_compare(int *a, int *b, int *c)
     int tidx = 0;
     int tidy = 0;
 
-    while (tidx < N) {
-        while (tidy < N) {
-            if (a[tidx * N + tidy] + b[tidx * N + tidy] > 50) {
-                c[tidx * N + tidy] = 1;
-            } else {
-                c[tidx * N + tidy] = 0;
-            }
-            tidy++;
-        }
+    while (tidx < N * N) {
+        //while (tidy < N) {
+            if (a[tidx/* * N + tidy*/] + b[tidx /** N + tidy*/] > 50) {
+                *c ^= (1u << tidx % 32);
+            } 
+         //   tidy++;
+       // }
         tidx++;
-        tidy = 0;
+      //  tidy = 0;
     }
 }
 
 __global__ void kernel(int *a, int *b, int *c) 
 {
     int tidx = blockIdx.x * blockDim.x + threadIdx.x;
-    int tidy = blockIdx.y * blockDim.y + threadIdx.y;
+    //int tidy = blockIdx.y * blockDim.y + threadIdx.y;
+    int offset = blockDim.x * gridDim.x;
+    __shared__ int temp[512];
+    temp[threadIdx.x] = 0;
+    __syncthreads();
+    
 
-    if (tidx < N && tidy < N) {
-        if ((a[tidx * N + tidy] + b[tidx * N + tidy]) > 50) {
-            c[tidx * N + tidy] = 1;
-        } else {  
-            c[tidx * N + tidy] = 0;
+    
+    while (tidx < N * N/* && tidy < N*/) {
+        if ((a[tidx/* * N + tidy*/] + b[tidx/* * N + tidy*/]) > 50) {
+            //atomicXor(c, 1u << tidx % 32);
+            atomicXor(&(temp[threadIdx.x]), 1u << tidx % 32);
+            //temp++;
+            //__syncthreads();
+            //atomicXor(c, temp[threadIdx.x]);
         }
+        tidx += offset;
+    }
+    
+    
+    // __syncthreads();       
+    if (temp[threadIdx.x] != 0) {
+        atomicXor(c, temp[threadIdx.x]);
     }
 }
 
-void allocate_memory(int** arr, int n, int m) 
+void allocate_memory(int** arr, int n, int m)
 {
     *arr = (int*) malloc(n * m * sizeof(int));
 }
 
-void deallocate_memory(int** arr, int n){
+void deallocate_memory(int** arr, int n)
+{
     free(*arr); 
+}
+
+int random() 
+{
+    return rand() % 51;
 }
 
 int main()
 {
-    int *a, *b, *c, *d;
+    int *a, *b;
+    int c = 0; 
+    int d = 0;
     int *dev_a, *dev_b, *dev_c;
     
+    srand (time(NULL));
+
     allocate_memory(&a, N, N);
     allocate_memory(&b, N, N);
-    allocate_memory(&c, N, N);
-    allocate_memory(&d, N, N);
 
     cudaMalloc((void**) &dev_a, N * N * sizeof(int));
     cudaMalloc((void**) &dev_b, N * N * sizeof(int));
-    cudaMalloc((void**) &dev_c, N * N * sizeof(int));
+    cudaMalloc((void**) &dev_c, sizeof(int));
  
     for (int i = 0; i < N; i++) {
         for (int j = 0; j < N; j++) {
-            a[i * N + j] = i + j;
-            b[i * N + j] = i * j;
+            a[i * N + j] = random();
+            b[i * N + j] = random();
         }   
     }
 
     cudaMemcpy(dev_a, a, N * N * sizeof(int), cudaMemcpyHostToDevice);
     cudaMemcpy(dev_b, b, N * N * sizeof(int), cudaMemcpyHostToDevice);
-    cudaMemcpy(dev_c, d, N * N * sizeof(int), cudaMemcpyHostToDevice);
+    cudaMemset(dev_c, 0, sizeof(int));
 
     clock_t end_cpu, start_cpu = clock();
 
-    cpu_compare(a, b, c);
+    cpu_compare(a, b, &c);
 
     end_cpu = clock();
 
@@ -100,16 +118,13 @@ int main()
     cudaEvent_t start, stop;
     float elapsedtime;
 
-    dim3 threadsPerBlock(16, 16);
-    dim3 numBlocks(N / threadsPerBlock.x, N / threadsPerBlock.y);
-
     cudaEventCreate(&start);
     cudaEventCreate(&stop); 
     cudaEventRecord(start, 0);
 
-    kernel<<<numBlocks, threadsPerBlock>>>(dev_a, dev_b, dev_c);
+    kernel<<<blocks_number, threads_number>>>(dev_a, dev_b, dev_c);
 
-    cudaMemcpy(d, dev_c, N * N * sizeof(int), cudaMemcpyDeviceToHost);
+    cudaMemcpy(&d, dev_c, sizeof(int), cudaMemcpyDeviceToHost);
 
     cudaStatus = cudaDeviceSynchronize();
     cudaEventRecord(stop, 0); 
@@ -120,12 +135,12 @@ int main()
 
     bool equal = isEqual(c, d);
 
-    printf("CPU time: %.1lf ms\nGPU time: %.1lf ms\nIs arrays equal: %s", 
-         (double) (end_cpu - start_cpu), elapsedtime, equal ? "yes" : "no");
+    printf("CPU: time = %.1lf ms, value = %i\nGPU: time = %.1lf ms, value = %i\nIs values equal: %s", 
+         (double) (end_cpu - start_cpu), c, elapsedtime, d, equal ? "yes" : "no");
 
     deallocate_memory(&a, N);
     deallocate_memory(&b, N);
-    deallocate_memory(&c, N);
+
     cudaFree(&dev_a);
     cudaFree(&dev_b);
     cudaFree(&dev_c);
